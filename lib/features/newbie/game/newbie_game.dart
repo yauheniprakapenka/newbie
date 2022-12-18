9e45f997e81a1fc0560d0714f178d3ff8e4a8846
yauheni.prakapenka <>
2022-12-18 12:04:13 +0300

    Add lantern animations

--- lib/features/newbie/game/newbie_game.dart
+++ lib/features/newbie/game/newbie_game.dart
@@ -14,6 +14,7 @@ import '../sprite_animations/bird/bird_component.dart';
 import '../sprite_animations/rabbit/rabbit_component.dart';
 import '../sprite_animations/sprite_animations.dart';
 import '../sprites/floor_indicator_component/floor_indicator_component.dart';
+import 'components/lantern_light_components.dart';
 import 'floor_manager.dart';
 import 'init_app_joystick.dart';
 import 'rooms/elevator_room.dart';
@@ -60,20 +61,7 @@ class NewbieGame extends FlameGame with HasCollisionDetection, HasTappables, Has
     BoyWithStickComponent()..position = Vector2(1833.296875, 1439.171925),
     GirlThrowSnowballComponent()..position = Vector2(1980, 1430),
     SantaClausComponent()..position = Vector2(1033.1171875, 1119.6016125),
-    LanternLightComponent()..position = Vector2(710.140625, 1364.13676875),
-    LanternLightComponent()..position = Vector2(2390.34765625, 1538.69145625),
-    LanternLightComponent()..position = Vector2(2744.75, 1314.44926875),
-    LanternLightComponent()..position = Vector2(3176.9296875, 1266.7422375),
-    LanternLightComponent()..position = Vector2(3302.28125, 1268.74614375),
-    LanternLightComponent()..position = Vector2(3302.28125, 1268.74614375),
-    LanternLightComponent()..position = Vector2(134.9921875, 1105.9453625),
-    LanternLightComponent()..position = Vector2(244.1875, 1380.51176875),
-    LanternLightComponent()..position = Vector2(244.69921875, 1588.74614375),
-    LanternLightComponent()..position = Vector2(135, 1587.99614375),
-    LanternLightComponent()..position = Vector2(1063.9453125, 1253.37505),
-    LanternLightComponent()..position = Vector2(1064.22265625, 1491.4766125),
-    LanternLightComponent()..position = Vector2(2263.0546875, 1393.50005),
-    LanternLightComponent()..position = Vector2(2630.9296875, 1538.4297375),
+
     RabbitComponent(initialePosition: Vector2(492.0625, 1547.25395625)),
     RabbitComponent(initialePosition: Vector2(991.65234375, 1389.08208125)),
     RabbitComponent(initialePosition: Vector2(1384.265625, 1601.2578625)),
@@ -87,63 +75,7 @@ class NewbieGame extends FlameGame with HasCollisionDetection, HasTappables, Has
     BirdComponent(initialePosition: Vector2(2222.796875, 1318.3359875)),
     BirdComponent(initialePosition: Vector2(2222.796875, 1318.3359875)),
 
-    ///
-    LanternLightComponent()..position = Vector2(310.30859375, 1727.984375),
-    LanternLightComponent()..position = Vector2(317.0625, 1715.22265625),
-    LanternLightComponent()..position = Vector2(324.2421875, 1726.921875),
-    //
-    LanternLightComponent()..position = Vector2(565.65234375, 1728.1171875),
-    LanternLightComponent()..position = Vector2(574.18359375, 1716.08203125),
-    LanternLightComponent()..position = Vector2(582.10546875, 1726.13671875),
-    //
-    LanternLightComponent()..position = Vector2(902.92578125, 1724.2734375),
-    LanternLightComponent()..position = Vector2(909.875, 1715.08203125),
-    LanternLightComponent()..position = Vector2(917.4921875, 1726.05078125),
-    //
-    LanternLightComponent()..position = Vector2(1206.80078125, 1728.14453125),
-    LanternLightComponent()..position = Vector2(1212.89453125, 1715.80078125),
-    LanternLightComponent()..position = Vector2(1221.73046875, 1721.89453125),
-    //
-    LanternLightComponent()..position = Vector2(1494.8984375, 1726.4609375),
-    LanternLightComponent()..position = Vector2(1500.3828125, 1714.42578125),
-    LanternLightComponent()..position = Vector2(1509.828125, 1725.6953125),
-    //
-    LanternLightComponent()..position = Vector2(1782.4921875, 1729.4296875),
-    LanternLightComponent()..position = Vector2(1790.09765625, 1717.06640625),
-    LanternLightComponent()..position = Vector2(1799.84375, 1726.81640625),
-    //
-    LanternLightComponent()..position = Vector2(2036.74609375, 1724.93359375),
-    LanternLightComponent()..position = Vector2(2047.5546875, 1715.46875),
-    LanternLightComponent()..position = Vector2(2055.171875, 1727.9609375),
-    //
-    LanternLightComponent()..position = Vector2(2309.79296875, 1727.95703125),
-    LanternLightComponent()..position = Vector2(2319.84375, 1713.453125),
-    LanternLightComponent()..position = Vector2(2325, 1726.84375),
-    //
-    LanternLightComponent()..position = Vector2(2611.61328125, 1725.453125),
-    LanternLightComponent()..position = Vector2(2619.53515625, 1714.33984375),
-    LanternLightComponent()..position = Vector2(2630.19921875, 1724.69921875),
-    //
-    LanternLightComponent()..position = Vector2(2935, 1725.4921875),
-    LanternLightComponent()..position = Vector2(2942.6171875, 1715.30859375),
-    LanternLightComponent()..position = Vector2(2951.43359375, 1725.97265625),
-    //
-    LanternLightComponent()..position = Vector2(3253.65625, 1727.15234375),
-    LanternLightComponent()..position = Vector2(3261.56640625, 1715.42578125),
-    LanternLightComponent()..position = Vector2(3270.40234375, 1724.56640625),
-    //
-    LanternLightComponent()..position = Vector2(3604.5, 1728.171875),
-    LanternLightComponent()..position = Vector2(3614.859375, 1715.2109375),
-    LanternLightComponent()..position = Vector2(3619.9921875, 1725.25),
-    //
-    LanternLightComponent()..position = Vector2(3894.62109375, 1726.95703125),
-    LanternLightComponent()..position = Vector2(3904.06640625, 1714.91796875),
-    LanternLightComponent()..position = Vector2(3911.68359375, 1726.484375),
-    //
-    LanternLightComponent()..position = Vector2(4197.6640625, 1726.00781255),
-    LanternLightComponent()..position = Vector2(4206.19140625, 1714.28125),
-    LanternLightComponent()..position = Vector2(4214.41796875, 1727.3828125),
-    //
+    ...getLanternLightComponents,
   ];
 
   // * Elevator
