506428c9230123da2562c2498258a20d20bfc500
yauheni.prakapenka <>
2022-12-22 23:16:50 +0300

    change config

--- lib/features/newbie/sprites/snowflake_component.dart
+++ lib/features/newbie/sprites/snowflake_component.dart
@@ -9,7 +9,7 @@ class SnowFlakeComponent extends SpriteComponent with HasGameRef<NewbieGame> {
   @override
   Future<void> onLoad() async {
     sprite = await gameRef.loadSprite(SnowflakeSprite.path);
-    size = SnowflakeSprite.srcSize * 0.07;
+    size = SnowflakeSprite.srcSize * 0.03;
     anchor = Anchor.center;
   }
 
@@ -20,7 +20,7 @@ class SnowFlakeComponent extends SpriteComponent with HasGameRef<NewbieGame> {
   }
 
   void _updateMovement() {
-    y += 1;
+    y += 0.3;
     if (y > _room401BottomY) {
       y = _room401TopY;
     }
